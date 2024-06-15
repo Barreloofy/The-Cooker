753d244149a7c4619aa76bc67e886c0987083882
Barreloofy <nilskofodhk@protonmail.com>
2024-06-15 16:48:13 -0700

    Continued work on checkList and refactored some stuff

---
+++ The Cooker/RecipeNonListViews.swift
@@ -0,0 +1,18 @@
+//
+//  RecipeNonListViews.swift
+//  The Cooker
+//
+//  Created by Nils on 6/15/24.
+//
+
+import SwiftUI
+
+struct RecipeNonListViews: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RecipeNonListViews()
+}
