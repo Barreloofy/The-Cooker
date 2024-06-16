07d08bddf25b85f8c67c935964197634ec90207e
Barreloofy <nilskofodhk@protonmail.com>
2024-06-15 17:11:42 -0700

    Implemented a step checklist for ingredients and directions.

---
+++ The Cooker/RecipeListView.swift
@@ -0,0 +1,18 @@
+//
+//  RecipeListView.swift
+//  The Cooker
+//
+//  Created by Nils on 6/15/24.
+//
+
+import SwiftUI
+
+struct RecipeListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RecipeListView()
+}
