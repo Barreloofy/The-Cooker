8ca9601078b6750fd7d53bb2f58102f89e0a6768
Barreloofy <nilskofodhk@protonmail.com>
2024-06-17 00:05:24 -0700

    Moved some stuff around in RecipeCategory and continued the foundation work of the add button feature

--- The Cooker/Views/RecipeCategory.swift
+++ The Cooker/Views/RecipeCategory.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 struct RecipeCategory: View {
-    
+    @State var isPresenting: Bool = false
     var body: some View {
         NavigationView {
             LazyVGrid(columns: Array(repeating: .init(.flexible()), count: 2)) {
@@ -16,10 +16,30 @@ struct RecipeCategory: View {
             }
             .padding(10)
             .frame(maxHeight: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, alignment: .top)
-            .navigationTitle("Category")
-            
-        }
+            //.navigationTitle("Category")
+            .toolbar {
+                ToolbarItem(placement: .topBarLeading) {
+                    Text("Category")
+                        .font(.largeTitle)
+                        .bold()
+                        .padding(.bottom, -1)
+                        .padding(.leading, -10)
+                }
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button(action: {
+                        isPresenting.toggle()
+                    }, label: {
+                        Image(systemName: "plus")
+                    })
+                    .foregroundStyle(.black)
+                    .padding(.bottom, -3)
+                }
+            }
+      }
         .navigationBarBackButtonHidden(true)
+        .sheet(isPresented: $isPresenting) {
+            Text("test")
+        }
     }
 }
 
