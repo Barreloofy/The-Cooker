c46d9bc6a1ad479837ccbadcab176128224da088
Barreloofy <nilskofodhk@protonmail.com>
2024-06-17 18:30:24 -0700

    Continued work on the add custom recipe feature 

---
+++ The Cooker/Model/UserData.swift
@@ -0,0 +1,8 @@
+//
+//  UserData.swift
+//  The Cooker
+//
+//  Created by Nils on 6/17/24.
+//
+
+import Foundation
---
+++ The Cooker/Views/AddRecipeView.swift
@@ -0,0 +1,18 @@
+//
+//  AddRecipeView.swift
+//  The Cooker
+//
+//  Created by Nils on 6/17/24.
+//
+
+import SwiftUI
+
+struct AddRecipeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddRecipeView()
+}
