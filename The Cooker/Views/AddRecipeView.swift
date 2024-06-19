01383b7622f4d60ded0112a1abc1aec14440825d
Barreloofy <nilskofodhk@protonmail.com>
2024-06-19 00:51:43 -0700

    some small changes

--- The Cooker/Views/AddRecipeView.swift
+++ The Cooker/Views/AddRecipeView.swift
@@ -10,32 +10,33 @@ import SwiftUI
 struct AddRecipeView: View {
     @ObservedObject var userData: UserData
     @State private var mainInformation: MainInformation = MainInformation(name: "", description: "", author: "", category: .breakfast)
-    @State private var ingredients = [Ingredient]()
-    @State private var ingredient = Ingredient(name: "", quantity: 0.0, unit: .none)
-    @State private var directions = [Direction]()
-    @State private var direction = Direction(description: "", isOptional: false)
+    @State private var ingredients = [Ingredient(name: "", quantity: 0.0, unit: .none)]
+    @State private var directions = [Direction(description: "", isOptional: false)]
+    //@State private var showTextField = false
     var body: some View {
         Form {
             Section(header: Text("Recipe Info")) {
-            TextField("Recipe Name", text: $mainInformation.name)
-            TextField("Recipe Author", text: $mainInformation.author)
-            TextField("Recipe Description", text: $mainInformation.description)
-            Picker("Recipe Category", selection: $mainInformation.category) {
-                ForEach(MainInformation.Category.allCases, id: \.self) {category in
-                    Text(category.rawValue)
-                }
-            }
-          }
-            Section(header: Text("Ingredients")) {
-                TextField("Ingredient Name", text: $ingredient.name)
-                Picker("Ingredient Unit", selection: $ingredient.unit) {
-                    ForEach(Ingredient.Unit.allCases, id: \.self) {unit in
-                        Text(unit.rawValue)
+                TextField("Recipe Name", text: $mainInformation.name)
+                TextField("Recipe Author", text: $mainInformation.author)
+                TextField("Recipe Description", text: $mainInformation.description)
+                Picker("Recipe Category", selection: $mainInformation.category) {
+                    ForEach(MainInformation.Category.allCases, id: \.self) {category in
+                        Text(category.rawValue)
                     }
                 }
-                if ingredient.unit != .none {
-                    TextField("Ingredient Quantity", value: $ingredient.quantity,format: .number).keyboardType(.decimalPad)
-                }
+            }
+            Section(header: Text("Ingredients")) {
+                ForEach($ingredients) {ingredientElement in
+                    VStack {
+                    TextField("Ingredient Name", text: ingredientElement.name)
+                        Picker("Ingredient Unit", selection: ingredientElement.unit) {
+                        ForEach(Ingredient.Unit.allCases, id: \.self) {unit in
+                            Text(unit.rawValue)
+                        }
+                    }
+                    TextField("Ingredient Quantity", value: ingredientElement.quantity, format: .number)
+                 }
+               }
             }
         }
     }
