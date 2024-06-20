6912c773eeb21ccbe3e6a42787f44d810af2063d
Barreloofy <nilskofodhk@protonmail.com>
2024-06-20 00:55:42 -0700

    Further refining the add custom recipe feature 

---
+++ The Cooker/Views/ForEachIngredient.swift
@@ -0,0 +1,18 @@
+//
+//  ForEachIngredient.swift
+//  The Cooker
+//
+//  Created by Nils on 6/20/24.
+//
+
+import SwiftUI
+
+struct ForEachIngredient: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ForEachIngredient()
+}
