61d696b359cbb55f28af12436e85b23e8e602658
Barreloofy <nilskofodhk@protonmail.com>
2024-06-22 17:53:24 -0700

    Added a button for users to add their own images to their custom recipes. As well as added a warning alert when the user wants to save a custom recipe with out having changed any data from its default values

---
+++ The Cooker/Views/AddImageButton.swift
@@ -0,0 +1,18 @@
+//
+//  AddImageButton.swift
+//  The Cooker
+//
+//  Created by Nils on 6/22/24.
+//
+
+import SwiftUI
+
+struct AddImageButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddImageButton()
+}
