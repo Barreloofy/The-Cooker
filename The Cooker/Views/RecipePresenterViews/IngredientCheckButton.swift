be43a0a61879a2f5e597695951b304bb4de00b97
Barreloofy <nilskofodhk@protonmail.com>
2024-06-28 00:13:02 -0700

    Continued work on various bug fixes and refactoring

---
+++ The Cooker/Views/RecipePresenterViews/IngredientCheckButton.swift
@@ -0,0 +1,18 @@
+//
+//  IngredientCheckButton.swift
+//  The Cooker
+//
+//  Created by Nils on 6/27/24.
+//
+
+import SwiftUI
+
+struct IngredientCheckButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    IngredientCheckButton()
+}
