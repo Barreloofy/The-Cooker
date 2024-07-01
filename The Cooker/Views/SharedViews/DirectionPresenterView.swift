13acda8a619f20927fa837898f8fc66dc5868c22
Barreloofy <nilskofodhk@protonmail.com>
2024-07-01 23:09:21 +0200

    refining the app

---
+++ The Cooker/Views/SharedViews/DirectionPresenterView.swift
@@ -0,0 +1,18 @@
+//
+//  DirectionPresenterView.swift
+//  The Cooker
+//
+//  Created by Nils on 7/1/24.
+//
+
+import SwiftUI
+
+struct DirectionPresenterView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DirectionPresenterView()
+}
---
+++ The Cooker/Views/SharedViews/IngredientPresenterView.swift
@@ -0,0 +1,18 @@
+//
+//  IngredientPresenterView.swift
+//  The Cooker
+//
+//  Created by Nils on 7/1/24.
+//
+
+import SwiftUI
+
+struct IngredientPresenterView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    IngredientPresenterView()
+}
